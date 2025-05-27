e1046f090620623c4c104cb92cc3d9bda5973193
Luana freitas <luana.freitas17@escola.pr.gov.br>
2025-05-27 15:59:56 -0300

    Add files via upload

---
+++ tiago-3t-2b-main/perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Crepúsculo',
+    'Em qual filme ocorre a grande batalha "final" com os Volturi?',
+    'Amanhecer – Parte 2.'
+)
+
+criaCartao(
+    'Crepúsculo',
+    'Qual é o nome completo da protagonista da saga Crepúsculo?',
+    ' Isabella Marie Swan.'
+
+)
+
+criaCartao(
+    'Crepúsculo',
+    'Em qual cidade Bella vai morar com seu pai no início do primeiro filme?',
+    'Forks, no estado de Washington.'
+)
+
+criaCartao(
+    'Crepúsculo',
+    'Qual é o nome da família de vampiros da qual Edward faz parte?',
+    'Cullen.'
+)
+
+criaCartao(
+    'Crepúsculo',
+    'Quem é o lobisomem que tem um triângulo amoroso com Bella e Edward?',
+    'Jacob Black.'
+)
+
+criaCartao(
+    'Crepúsculo',
+    'Em qual filme Bella se transforma em vampira?',
+    'Amanhecer – Parte 1 (no final do filme).'
+)
+
+criaCartao(
+    'Crepúsculo',
+    'Qual o nome da filha de Bella e Edward?',
+    'Renesmee Cullen..'
+)
+
+criaCartao(
+    'Crepúsculo',
+    'Quem é o líder dos Volturi, o clã de vampiros que impõe as leis?',
+    'Aro.'
+)
+
+criaCartao(
+    'Crepúsculo',
+    'Qual é o dom especial de Edward como vampiro?',
+    'Ele consegue ler mentes (com exceção da Bella, enquanto ela é humana).'
+)
